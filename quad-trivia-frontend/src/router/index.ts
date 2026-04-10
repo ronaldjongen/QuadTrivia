db53f65956eac09692996a2b791856d877960a07
ronaldjongen <ronaldjongen@mailbox.org>
2026-04-10 13:55:50 +0200

    Refactor: Routing

--- quad-trivia-frontend/src/router/index.ts
+++ quad-trivia-frontend/src/router/index.ts
@@ -4,15 +4,17 @@ import QuizView from '../views/QuizView.vue'
 import ResultView from '../views/ResultView.vue'
 import { useAuthStore } from '../stores/authenticationStore'
 import { RoutePath } from './routePath'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', redirect: RoutePath.Quizroute },
-    { path: RoutePath.Loginroute,name:'login', component: LoginView },
-    { path: RoutePath.Quizroute,name: 'quiz', component: QuizView , meta:{ requiresAuth: true}},
-    { path: RoutePath.Resultroute, component: ResultView, meta:{ requiresAuth: true} },
+    { path: '/', redirect: RoutePath.Quiz },
+    { path: RoutePath.Login, name: 'login', component: LoginView },
+    { path: RoutePath.Quiz, name: 'quiz', component: QuizView, meta: { requiresAuth: true } },
+    { path: RoutePath.Result, component: ResultView, meta: { requiresAuth: true } },
   ],
 })
+
 // before each route check if user is authenticated if not redirect to login if needed
 router.beforeEach(async (to) => {
   const authStore = useAuthStore()
@@ -22,11 +24,12 @@ router.beforeEach(async (to) => {
   }
 
   if (to.meta.requiresAuth && !authStore.isAuthenticated) {
-    return RoutePath.Loginroute
+    return RoutePath.Login
   }
 
-  if (to.path === RoutePath.Loginroute && authStore.isAuthenticated) {
-    return RoutePath.Quizroute
+  if (to.path === RoutePath.Login && authStore.isAuthenticated) {
+    return RoutePath.Quiz
   }
 })
+
 export default router
